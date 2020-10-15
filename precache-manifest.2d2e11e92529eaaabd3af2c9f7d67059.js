a7fd9daf0db242b4fc4f13200d15e8b020e07dcf
Guilherme <guilherme_070@hotmail.com>
2020-10-15 20:03:08 -0300

    Updates

---
+++ precache-manifest.2d2e11e92529eaaabd3af2c9f7d67059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07999182fd3136fdf8d35ce018d0299e",
+    "url": "/deputados-api/index.html"
+  },
+  {
+    "revision": "01db29f3a0f1328cc0b6",
+    "url": "/deputados-api/static/css/main.61ec5f47.chunk.css"
+  },
+  {
+    "revision": "443b7dd57e7a0caebbb7",
+    "url": "/deputados-api/static/js/2.70590f2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deputados-api/static/js/2.70590f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01db29f3a0f1328cc0b6",
+    "url": "/deputados-api/static/js/main.dd93ca83.chunk.js"
+  },
+  {
+    "revision": "f84a59f2000ae3ba5d94",
+    "url": "/deputados-api/static/js/runtime-main.1bbd5da6.js"
+  }
+]);
